9e77b659f8589d2e7c80fc3bc2d1d49f6b6f8117
AndoNando15 <andonando158@gmail.com>
2023-06-12 17:01:37 +0700

    Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import styles from './App.module.css';
+import Button from './Component/Button';
 
 function App() {
   return (
@@ -26,7 +27,9 @@ function App() {
           </a>
         </div>
       </nav>
-      <h1>Praktikum A2 Pagi</h1>
+      <h1 className="text-2xl hover:font-bold hover:pointer">Praktikum A2 Pagi</h1>
+      <Button className="bg-blue-400 text-white  hover:bg-blue-500">Button 1</Button>
+      <Button className="bg-blue-400 text-white  hover:bg-blue-500">Button 2</Button>
     </>
   );
 }
